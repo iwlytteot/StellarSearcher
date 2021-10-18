9d6342dd201d7cb0605af0e5c607d498c32b4749
Leo Tovarys <xtovarys@fi.muni.cz>
2021-10-19 01:47:35 +0200

    constraint save implemented

--- src/main/java/controller/ConstraintWindowController.java
+++ src/main/java/controller/ConstraintWindowController.java
@@ -1,11 +1,19 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ConstraintWindowController {
     @FXML
     public Label textLabel;
+    @FXML
+    public TextField input;
+    @FXML
+    public Button confirmButton;
 
     private String name;
     private String constraint;
@@ -13,5 +21,11 @@ public class ConstraintWindowController {
     public void init(String name) {
         this.name = name;
         textLabel.setText("Constraint: " + this.name);
+    }
+
+    public void confirmAction(ActionEvent actionEvent) {
+        constraint = input.getText();
+        var stage = (Stage) confirmButton.getScene().getWindow();
+        stage.hide();
     }
 }
