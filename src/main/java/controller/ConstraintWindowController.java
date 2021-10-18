43c302ec9a03e754c405a8cafba76805cdea3039
Leo Tovarys <xtovarys@fi.muni.cz>
2021-10-19 01:38:08 +0200

    introducing constraint window for filter

--- src/main/java/controller/ConstraintWindowController.java
+++ src/main/java/controller/ConstraintWindowController.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class ConstraintWindowController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class ConstraintWindowController {
+    @FXML
+    public Label textLabel;
+
+    private String name;
+    private String constraint;
+
+    public void init(String name) {
+        this.name = name;
+        textLabel.setText("Constraint: " + this.name);
+    }
 }
--- src/main/java/controller/FilterWindowController.java
+++ src/main/java/controller/FilterWindowController.java
@@ -3,11 +3,21 @@ package controller;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import model.Data;
 import model.Table;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class FilterWindowController {
     @FXML
@@ -15,12 +25,13 @@ public class FilterWindowController {
     @FXML
     public ListView<String> listViewUsed;
 
-    public void constraintWindowAction(MouseEvent mouseEvent) {
-    }
+    private final HashMap<String, Stage> constraints = new HashMap<>();
 
     public void init(Table table) {
         listViewAvailable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         listViewUsed.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        listViewAvailable.setOnMouseClicked(eventHandlerAvailable);
+        listViewUsed.setOnMouseClicked(eventHandlerUsed);
 
         listViewAvailable.getItems().addAll(table.getColumns().keySet());
     }
@@ -37,5 +48,48 @@ public class FilterWindowController {
         var selected = listViewUsed.getSelectionModel().getSelectedItems();
         listViewAvailable.getItems().addAll(selected);
         listViewUsed.getItems().removeAll(selected);
+    }
+
+    private final EventHandler<MouseEvent> eventHandlerAvailable = new EventHandler<>() {
+        @Override
+        public void handle(MouseEvent event) {
+            if (event.getClickCount() == 2) {
+                var selected = listViewAvailable.getSelectionModel().getSelectedItem();
+                constraints.putIfAbsent(selected, initFxml("Constraint window", selected));
+                constraints.get(selected).show();
+            }
+        }
+    };
+
+    private final EventHandler<MouseEvent> eventHandlerUsed = new EventHandler<>() {
+        @Override
+        public void handle(MouseEvent event) {
+            if (event.getClickCount() == 2) {
+                var selected = listViewUsed.getSelectionModel().getSelectedItem();
+                constraints.putIfAbsent(selected, initFxml("Constraint window", selected));
+                constraints.get(selected).show();
+            }
+        }
+    };
+
+    private Stage initFxml(String stageTitle, String constraintName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ConstraintWindow.fxml"));
+            Parent root = loader.load();
+
+            ConstraintWindowController constraintWindowController = loader.getController();
+            constraintWindowController.init(constraintName);
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.setTitle(stageTitle);
+            return stage;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
