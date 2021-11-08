ee2d80b58d759a31bee7a970884479719b32bb7d
Leo Tovarys <xtovarys@fi.muni.cz>
2021-11-08 14:30:48 +0100

    importing data to TableView, prototype

--- src/main/java/controller/ResultWindowController.java
+++ src/main/java/controller/ResultWindowController.java
@@ -1,16 +1,19 @@
 package controller;
 
-import cds.savot.model.SavotField;
-import cds.savot.model.SavotResource;
-import cds.savot.model.SavotTable;
-import cds.savot.model.SavotVOTable;
+import cds.savot.model.*;
 import cds.savot.pull.SavotPullEngine;
 import cds.savot.pull.SavotPullParser;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
 
 public class ResultWindowController {
     @FXML
@@ -22,16 +25,29 @@ public class ResultWindowController {
         var resources = sv.getResources();
         for (var item : resources.getItems()) {
             var s = (SavotResource) item;
-            System.out.println("cat: " + s.getName());
             var tables = s.getTables();
             for (var table : tables.getItems()) {
                 var t = (SavotTable) table;
                 var tableTab = new Tab(t.getName());
 
-                var tableView = new TableView<>();
+                var tableView = new TableView<List<String>>();
                 for (int i = 0; i < t.getFields().getItemCount(); ++i) {
                     var field = (SavotField) t.getFields().getItemAt(i);
-                    tableView.getColumns().add(new TableColumn<>(field.getName()));
+                    var column = new TableColumn<List<String>, String>(field.getName());
+                    int finalI = i;
+                    column.setCellValueFactory(v -> new SimpleStringProperty(v.getValue().get(finalI)));
+                    tableView.getColumns().add(column);
+                }
+
+                var data = t.getData().getTableData();
+                for (var trItem : data.getTRs().getItems()) {
+                    var tr = (SavotTR) trItem;
+                    var row = new ArrayList<String>();
+                    for (var td : tr.getTDs().getItems()) {
+                        var f = (SavotTD) td;
+                        row.add(f.getContent());
+                    }
+                    tableView.getItems().addAll(row);
                 }
                 tableTab.setContent(tableView);
                 tabPane.getTabs().add(tableTab);
