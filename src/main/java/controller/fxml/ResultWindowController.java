03542e6f745a038b42451ceb21fc63ebd90d823b
Leo Tovarys <xtovarys@fi.muni.cz>
2022-04-08 15:04:55 +0200

    changing doc

--- src/main/java/controller/fxml/ResultWindowController.java
+++ src/main/java/controller/fxml/ResultWindowController.java
@@ -131,7 +131,7 @@ public class ResultWindowController {
 
     /**
      * Method that opens the directory chooser and then proceeds to export data such that file name is enumerated as
-     * 'x.txt', where x is in range <0, p>, where 'p' is number of exported Tabs.
+     * 'x.txt', where x is in range <0, p> and 'p' is number Tabs to be exported.
      */
     public void exportData() {
         if (exportWindowEventHandler.getStage() == null) {
