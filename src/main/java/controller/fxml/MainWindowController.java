0a15aa3fe8dcb2cfa41584e0b2a81db514f8420b
Leo Tovarys <xtovarys@fi.muni.cz>
2022-05-14 10:41:13 +0200

    clean up

--- src/main/java/controller/fxml/MainWindowController.java
+++ src/main/java/controller/fxml/MainWindowController.java
@@ -8,7 +8,6 @@ import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.control.*;
